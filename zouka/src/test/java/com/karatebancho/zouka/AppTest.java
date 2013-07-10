d41ee72fe4635fde64739f7328b0f90ad535abfe
ysobj <litailang+ysobj@gmail.com>
2013-07-10 23:28:14 +0900

    テストケースの追加

--- zouka/src/test/java/com/karatebancho/zouka/AppTest.java
+++ zouka/src/test/java/com/karatebancho/zouka/AppTest.java
@@ -1,34 +1,45 @@
 package com.karatebancho.zouka;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import com.karatebancho.zouka.impl.HiraKataFilter;
+import com.karatebancho.zouka.impl.MorphologicalFilter;
+import com.karatebancho.zouka.impl.RomanFilter;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-	/**
-	 * Create the test case
-	 * 
-	 * @param testName
-	 *            name of the test case
-	 */
-	public AppTest(String testName) {
-		super(testName);
+public class AppTest {
+	protected static List<Filter> filters = new ArrayList<>();
+	{
+		filters.add(new MorphologicalFilter());
+		filters.add(new HiraKataFilter());
+		filters.add(new RomanFilter());
 	}
 
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
-	}
-
-	/**
-	 * Rigourous Test :-)
-	 */
+	@Test
 	public void testApp() {
-		assertTrue(true);
+		assertThat(filter("笑止高齢化"), is(filter("少子高齢化")));
+		assertThat(filter("しゃ皆保険"), is(filter("社会保険")));
+		// assertThat(filter("滑稽銀"), is(filter("国会議員")));
+		// Expected: is "koxtukaigiinn"
+		// but: was "koxtukeiginn"
+		System.out.println(StringUtils.getLevenshteinDistance("koxtukaigiinn",
+				"koxtukeiginn"));
+	}
+
+	protected String filter(String original) {
+		String tmp = original;
+		for (Filter filter : filters) {
+			tmp = filter.filter(tmp);
+		}
+		return tmp;
 	}
 }

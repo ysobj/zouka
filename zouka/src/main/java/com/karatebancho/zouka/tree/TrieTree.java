5a5bbaa596db6e80a52195a93cbe3ab9c77e51d1
ysobj <litailang.ysobj@gmail.com>
2013-07-16 13:02:07 +0900

    trie木で実装してみるつもり。

---
+++ zouka/src/main/java/com/karatebancho/zouka/tree/TrieTree.java
@@ -0,0 +1,82 @@
+package com.karatebancho.zouka.tree;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class TrieTree<V> implements Map<String, V> {
+	protected int count;
+
+	@Override
+	public int size() {
+		return count;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(String key, V value) {
+		count++;
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends V> m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<String> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<String, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ zouka/src/test/java/com/karatebancho/zouka/tree/TreeTest.java
@@ -0,0 +1,22 @@
+package com.karatebancho.zouka.tree;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TreeTest {
+
+	@Test
+	public void test() {
+		TrieTree<String> root = new TrieTree<>();
+		// 文字列を追加できる
+		root.put("toukyou", "東京");
+		// 文字列の数を確認できる
+		assertThat(root.size(), is(1));
+		// 文字列を取得できる
+		String actual = root.get("toukyou");
+		assertThat(actual, is("東京"));
+	}
+
+}
